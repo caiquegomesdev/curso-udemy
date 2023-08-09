ea43b8cb2db0f7f86bd2312f8617a22cb86beb4e
Caíque Gomes <111620241+kionstack@users.noreply.github.com>
2023-08-08 21:49:30 -0300

    Curso Udemy

---
+++ esnext/operadorSpreadRest.js
@@ -0,0 +1,13 @@
+// operador... Rest(juntar)/ spread (espelhar)
+//usar rest com parametro de funcao
+
+//usar spread com objeto
+
+const funcionario = {nome: 'Maria', salario: 12345.99}
+const clone = {ativo: true, ...funcionario}
+console.log(clone)
+
+//usar spread com array
+const grupoA= ['Joao', 'Pedro', 'Gloria']
+const grupoFinal = ['Maria', ...grupoA, 'Rafaela']
+console.log(grupoFinal)
